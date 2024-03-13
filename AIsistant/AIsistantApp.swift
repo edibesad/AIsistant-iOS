e1a25c16ac3c1deae603c3beffbf14b966f7730a
Edib Esad Galip <edibesad@gmail.com>
2024-03-13 16:34:28 +0300

    Initial Commit

---
+++ AIsistant/AIsistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIsistantApp.swift
+//  AIsistant
+//
+//  Created by Edib Esad Galip on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AIsistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
